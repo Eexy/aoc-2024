f172ef29dbb3d80d87a7e42b2bbe24174f87c592
eexy <eexdhe@gmail.com>
2026-04-20 19:06:34 +0200

    feat: add day 1 part 1

---
+++ day_1/src/main.rs
@@ -0,0 +1,45 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    process,
+};
+
+fn main() {
+    let file = match File::open("input.txt") {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("unable to read file: {err}");
+            process::exit(1);
+        }
+    };
+
+    let reader = BufReader::new(file);
+    let mut l_list = vec![];
+    let mut r_list = vec![];
+
+    for line in reader.lines() {
+        let line = match line {
+            Ok(l) => l,
+            Err(err) => {
+                eprintln!("unable to read line: {err}");
+                process::exit(1);
+            }
+        };
+
+        let mut parsed = line
+            .split_whitespace()
+            .map(|value| value.parse::<i32>().unwrap());
+
+        if let (Some(l_value), Some(r_value)) = (parsed.next(), parsed.next()) {
+            l_list.push(l_value);
+            r_list.push(r_value);
+        }
+    }
+
+    l_list.sort();
+    r_list.sort();
+
+    let difference: i32 = l_list.iter().zip(&r_list).map(|(a, b)| (b - a).abs()).sum();
+
+    dbg!(difference);
+}
