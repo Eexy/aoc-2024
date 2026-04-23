b94ab22f006c52258ae95272c59c8f8d82b31494
eexy <eexdhe@gmail.com>
2026-04-23 22:04:25 +0200

    feat: add day 4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -21,116 +21,31 @@ fn main() {
         .collect::<Vec<_>>();
 
     let mut total = 0;
-    // search horizontally
-    let horizontal_foward_iter = content.iter().enumerate();
-    for (index, line) in horizontal_foward_iter {
-        let iter = line.iter().enumerate();
-        for (i, ch) in iter {
-            if ch == &'X' {
-                let forward_str = line
-                    .get(i..=i + 3)
-                    .map(|s| s.iter().collect::<String>())
-                    .unwrap_or(String::from(""));
-                if forward_str == "XMAS" {
-                    total += 1;
-                }
 
-                if i >= 3 {
-                    let backward_str = line
-                        .get(i - 3..=i)
-                        .map(|s| s.iter().collect::<String>())
-                        .unwrap_or(String::from(""));
-                    if backward_str == "SAMX" {
-                        total += 1;
-                    }
-                }
-            }
-        }
-    }
-    // search vertical
-    let vertical_iter = content.iter().enumerate();
-    for (index, line) in vertical_iter {
-        let iter = line.iter().enumerate();
-        for (i, ch) in iter {
-            if ch == &'X' {
-                if index > 2 {
-                    let mut top_str = String::from("X");
-                    top_str.push(content[index - 1][i]);
-                    top_str.push(content[index - 2][i]);
-                    top_str.push(content[index - 3][i]);
-
-                    if top_str == "XMAS" {
-                        total += 1;
-                    }
-                }
-
-                if index < content.len() - 3 {
-                    let mut bottom_str = String::from("X");
-                    bottom_str.push(content[index + 1][i]);
-                    bottom_str.push(content[index + 2][i]);
-                    bottom_str.push(content[index + 3][i]);
-
-                    if bottom_str == "XMAS" {
-                        total += 1;
-                    }
-                }
-            }
-        }
-    }
-    // search diagonal
     let diagonal_iter = content.iter().enumerate();
     for (index, line) in diagonal_iter {
         let iter = line.iter().enumerate();
         for (i, ch) in iter {
-            // let is_valid_diag_top_right = index - 3 >= 0 && i + 3 > line.len();
-            // let is_valid_diag_top_left = index > 2 && index - 3 <= 0 && i - 3 >= 0;
-            // let is_valid_diag_bottom_left = index + 3 < content.len() && i - 3 >= 0;
-            //diag bottom righ
-            if ch == &'X' {
-                let is_valid_diag_bottom_right = index + 3 < content.len() && i + 3 < line.len();
-                if is_valid_diag_bottom_right {
-                    let mut diag_bottom_right_str = String::from("X");
-                    diag_bottom_right_str.push(content[index + 1][i + 1]);
-                    diag_bottom_right_str.push(content[index + 2][i + 2]);
-                    diag_bottom_right_str.push(content[index + 3][i + 3]);
+            if ch == &'A' {
+                let is_valid_height_range = index > 0 && index < content.len() - 1;
+                let is_valid_width_range = i > 0 && i < line.len() - 1;
 
-                    if diag_bottom_right_str == "XMAS" {
-                        total += 1;
-                    }
-                }
+                if is_valid_width_range && is_valid_height_range {
+                    // diag top left to bottom right
+                    let first_lhs_ch = content[index - 1][i - 1];
+                    let last_lhs_ch = content[index + 1][i + 1];
 
-                let is_valid_diag_top_right = index > 2 && index - 3 >= 0 && i + 3 < line.len();
-                if is_valid_diag_top_right {
-                    let mut diag_top_right_str = String::from("X");
-                    diag_top_right_str.push(content[index - 1][i + 1]);
-                    diag_top_right_str.push(content[index - 2][i + 2]);
-                    diag_top_right_str.push(content[index - 3][i + 3]);
+                    let lhs = (first_lhs_ch == 'S' && last_lhs_ch == 'M')
+                        || (first_lhs_ch == 'M' && last_lhs_ch == 'S');
 
-                    if diag_top_right_str == "XMAS" {
-                        total += 1;
-                    }
-                }
+                    //diag top right to bottom left
+                    let first_rhs_ch = content[index - 1][i + 1];
+                    let last_rhs_ch = content[index + 1][i - 1];
 
-                let is_valid_diag_top_left = index > 2 && index - 3 >= 0 && i > 2 && i - 3 >= 0;
-                if is_valid_diag_top_left {
-                    let mut diag_top_left_str = String::from("X");
-                    diag_top_left_str.push(content[index - 1][i - 1]);
-                    diag_top_left_str.push(content[index - 2][i - 2]);
-                    diag_top_left_str.push(content[index - 3][i - 3]);
+                    let rhs = (first_rhs_ch == 'S' && last_rhs_ch == 'M')
+                        || (first_rhs_ch == 'M' && last_rhs_ch == 'S');
 
-                    if diag_top_left_str == "XMAS" {
-                        total += 1;
-                    }
-                }
-
-                let is_valid_diag_bottom_left = index + 3 < content.len() && i > 2 && i - 3 >= 0;
-                if is_valid_diag_bottom_left {
-                    let mut diag_top_left_str = String::from("X");
-                    diag_top_left_str.push(content[index + 1][i - 1]);
-                    diag_top_left_str.push(content[index + 2][i - 2]);
-                    diag_top_left_str.push(content[index + 3][i - 3]);
-
-                    if diag_top_left_str == "XMAS" {
+                    if rhs && lhs {
                         total += 1;
                     }
                 }
