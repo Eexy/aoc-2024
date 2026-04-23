9af2b87bdc588e4f8f5f13b580be53301c6b64a9
eexy <eexdhe@gmail.com>
2026-04-23 22:10:35 +0200

    chore: optimize code by directly starting at one to avoid bound check

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -22,32 +22,26 @@ fn main() {
 
     let mut total = 0;
 
-    let diagonal_iter = content.iter().enumerate();
-    for (index, line) in diagonal_iter {
-        let iter = line.iter().enumerate();
-        for (i, ch) in iter {
-            if ch == &'A' {
-                let is_valid_height_range = index > 0 && index < content.len() - 1;
-                let is_valid_width_range = i > 0 && i < line.len() - 1;
+    for col in 1..content.len() - 1 {
+        let line = &content[col];
+        for row in 1..line.len() - 1 {
+            let ch = line[row];
+            if ch == 'A' {
+                let first_lhs_ch = content[col - 1][row - 1];
+                let last_lhs_ch = content[col + 1][row + 1];
 
-                if is_valid_width_range && is_valid_height_range {
-                    // diag top left to bottom right
-                    let first_lhs_ch = content[index - 1][i - 1];
-                    let last_lhs_ch = content[index + 1][i + 1];
+                let lhs = (first_lhs_ch == 'S' && last_lhs_ch == 'M')
+                    || (first_lhs_ch == 'M' && last_lhs_ch == 'S');
 
-                    let lhs = (first_lhs_ch == 'S' && last_lhs_ch == 'M')
-                        || (first_lhs_ch == 'M' && last_lhs_ch == 'S');
+                //diag top right to bottom left
+                let first_rhs_ch = content[col - 1][row + 1];
+                let last_rhs_ch = content[col + 1][row - 1];
 
-                    //diag top right to bottom left
-                    let first_rhs_ch = content[index - 1][i + 1];
-                    let last_rhs_ch = content[index + 1][i - 1];
+                let rhs = (first_rhs_ch == 'S' && last_rhs_ch == 'M')
+                    || (first_rhs_ch == 'M' && last_rhs_ch == 'S');
 
-                    let rhs = (first_rhs_ch == 'S' && last_rhs_ch == 'M')
-                        || (first_rhs_ch == 'M' && last_rhs_ch == 'S');
-
-                    if rhs && lhs {
-                        total += 1;
-                    }
+                if rhs && lhs {
+                    total += 1;
                 }
             }
         }
